8966051b089f7f260a5e3be28abbe8c1304c0e19
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-07-16 16:36:33 +0200

    Play with castShadow & receive shadow

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,27 @@ import { Canvas } from "@react-three/fiber";
 function App() {
   return (
     <>
-      <Canvas camera={{ position: [0, 3, 3] }}>
+      <Canvas camera={{ position: [0, 3, 3] }} shadows>
         <OrbitControls />
         <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 5, 5]} intensity={0.5} />
-        <directionalLight position={[-5, 5, 5]} intensity={0.5} color="red" />
+        <directionalLight position={[5, 5, 5]} intensity={0.5} castShadow />
+        <directionalLight
+          position={[-5, 5, 5]}
+          intensity={0.5}
+          color="red"
+          castShadow
+        />
 
-        <mesh rotation-y={Math.PI / 4}>
+        <mesh position={[1, 1, 1]} castShadow>
+          <sphereGeometry args={[0.5, 32, 32]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+        <mesh rotation-y={Math.PI / 4} castShadow receiveShadow>
           <boxGeometry />
           <meshStandardMaterial color="white" />
         </mesh>
 
-        <mesh rotation-x={-Math.PI / 2} position-y={-0.5}>
+        <mesh rotation-x={-Math.PI / 2} position-y={-0.5} receiveShadow>
           <planeGeometry args={[5, 5]} />
           <meshStandardMaterial color="white" />
         </mesh>
